02fb99716b50a60ae332ca2042b0f54cd3892216
Toru Furuya <t.furuya825@gmail.com>
2014-11-09 22:09:43 +0900

    Initial Commit

---
+++ MyFirstSwiftApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyFirstSwiftApp
+//
+//  Created by Toru Furuya on 2014/11/09.
+//  Copyright (c) 2014年 edu.myself. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
